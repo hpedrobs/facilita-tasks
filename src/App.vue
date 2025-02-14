887d3ad381c5fb4a3fd3b6bf45b68e26b26b87d4
Pedro Henrique <hpedrobs@gmail.com>
2025-02-14 05:27:02 -0300

    feat: criação da caixa da dashboard.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,76 @@
-<script setup>
+<script>
 import { RouterView } from "vue-router"
+import { mapGetters } from "vuex"
+import LinkSidebar from "@/components/LinkSidebar.vue"
+
+// Icones
+import DashboardIcon from "@/assets/icons/dashboard-icon.svg"
+import PlusCircleIcon from "@/assets/icons/plus-circle-icon.svg"
+import ConfigIcon from "@/assets/icons/config-icon.svg"
+import BarsIcon from "@/assets/icons/bars-icon.svg"
+import AvatarIcon from "@/assets/icons/avatar-icon.svg"
+
+export default {
+    components: {
+        RouterView,
+        LinkSidebar,
+    },
+    data() {
+        return {
+            DashboardIcon,
+            PlusCircleIcon,
+            ConfigIcon,
+            BarsIcon,
+            AvatarIcon,
+            isVisible: false,
+        }
+    },
+    computed: {
+        menuStyle() {
+            return this.isVisible ? "display: block;" : "display: none;"
+        },
+        isLoginPage() {
+            return this.$route.path === "/login"
+        },
+        ...mapGetters({ user: "getUser" }),
+    },
+}
 </script>
-
 <template>
-    <main class="w-full min-h-dvh">
-        <RouterView />
-    </main>
-</template>
+    <div class="box-head">
+        <!-- Sidebar -->
+        <nav class="sidebar" v-if="!isLoginPage">
+            <div class="logo">
+                <img src="@/assets/images/logo-small.jpg" width="50" alt="Logo Pequena" />
+            </div>
 
-<style scoped></style>
+            <button type="button" class="menu-toggle" @click="isVisible = !isVisible">
+                <img :src="BarsIcon" alt="Icone Menu Toggle" />
+            </button>
+
+            <ul class="menu" :style="menuStyle">
+                <LinkSidebar text="Dashboard" :icon="DashboardIcon" :active="true" />
+                <LinkSidebar text="Tarefas" :icon="PlusCircleIcon" />
+                <LinkSidebar text="Configurações" :icon="ConfigIcon" />
+            </ul>
+        </nav>
+
+        <div class="wrapper">
+            <!-- Topbar -->
+            <header class="top-bar" v-if="!isLoginPage">
+                <div class="user-info">
+                    <img :src="AvatarIcon" alt="Usuário" />
+                    <div class="user-data">
+                        <span class="title">{{ user.username }}</span>
+                        <span class="subtitle">Front-end Developer</span>
+                    </div>
+                </div>
+            </header>
+
+            <!-- Main -->
+            <main class="content">
+                <RouterView />
+            </main>
+        </div>
+    </div>
+</template>
---
+++ src/components/LinkSidebar.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: "LinkSidebar",
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        icon: {
+            type: String,
+            required: true,
+        },
+        active: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+    computed: {
+        isActive() {
+            return this.active ? "active" : ""
+        },
+    },
+}
+</script>
+
+<template>
+    <li :class="isActive">
+        <div class="icon">
+            <img :src="icon" :alt="text" />
+        </div>
+        <a href="#">{{ text }}</a>
+    </li>
+</template>
