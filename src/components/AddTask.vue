2d0eeb1e98efa3c94cdc020c49778c757cf556be
Pedro Henrique <hpedrobs@gmail.com>
2025-02-15 03:31:10 -0300

    feat: criação do componente de adicionar tarefa.

---
+++ src/components/AddTask.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <button class="icon-circle" @click="showModal = true">
+            <img :src="PlusIcon" width="24" height="31" />
+        </button>
+
+        <Modal :isOpen="showModal" @close="showModal = false">
+            <form class="form w-full" action="/" method="get" @submit.prevent="addTask">
+                <h3>Cadastrar Tarefa</h3>
+
+                <div class="form-group w-full">
+                    <label for="">Título:</label>
+                    <input type="text" v-model="task.title" />
+                </div>
+
+                <div class="form-group">
+                    <label for="">Descrição:</label>
+                    <textarea v-model="task.description" />
+                </div>
+
+                <div class="radio-group">
+                    <label class="radio-container">
+                        <input type="radio" value="urgente" v-model="task.status" />
+                        <span class="radio-check"></span>
+                        Urgente
+                    </label>
+                    <label class="radio-container">
+                        <input type="radio" value="importante" v-model="task.status" />
+                        <span class="radio-check"></span>
+                        Importante
+                    </label>
+                </div>
+
+                <button type="submit" class="btn btn-green" :disabled="!task.title || !task.status">
+                    Entrar
+                </button>
+            </form>
+        </Modal>
+    </div>
+</template>
+
+<script>
+import Modal from "./Modal.vue"
+import PlusIcon from "@/assets/icons/plus-icon.svg"
+
+export default {
+    name: "AddTask",
+    components: {
+        Modal,
+    },
+    data() {
+        return {
+            PlusIcon,
+            showModal: false,
+            task: {
+                title: "",
+                description: "",
+                status: "",
+            },
+        }
+    },
+    methods: {
+        addTask() {
+            this.$store.commit("addTask", this.task)
+            this.resetTask()
+            this.showModal = false
+        },
+        resetTask() {
+            this.task = {
+                title: "",
+                description: "",
+                status: "",
+            }
+        },
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+h3
+ color #283848
+ font-size 25px
+ font-weight bold
+
+.icon-circle
+    position fixed
+    bottom 20px
+    right 20px
+    margin 0
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="isOpen" class="modal-overlay" @click.self="close">
+        <div class="modal-content">
+            <div class="close-btn-content">
+                <button @click="close">
+                    <img :src="CloseIcon" height="15" width="15" />
+                </button>
+            </div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import CloseIcon from "@/assets/icons/close-icon.svg"
+
+export default {
+    name: "ModalGlobal",
+    props: {
+        isOpen: Boolean,
+    },
+    data() {
+        return {
+            CloseIcon,
+        }
+    },
+    methods: {
+        close() {
+            this.$emit("close")
+        },
+    },
+}
+</script>
